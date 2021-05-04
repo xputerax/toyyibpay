8ab7fcce825ce6ec3d46c96956f99abc6b6da9f1
Aiman Daniel <aiman2301@gmail.com>
2021-05-04 12:39:25 +0800

    Use custom response object
    - isUnauthorized method now will also check the response body instead of relying on HTTP status code
    - Add validator for category code
    - getBody will return trimmed body
    - Change Exception messages

--- src/Request.php
+++ src/Request.php
@@ -2,9 +2,21 @@
 
 namespace AimanDaniel\ToyyibPay;
 
-use \Laravie\Codex\Request as BaseRequest;
+use Laravie\Codex\Request as BaseRequest;
+use Laravie\Codex\Contracts\Response as ResponseContract;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class Request extends BaseRequest
 {
-    // 
+    /**
+     * Resolve the responder class.
+     *
+     * @param  \Psr\Http\Message\ResponseInterface  $message
+     *
+     * @return \Laravie\Codex\Contracts\Response
+     */
+    protected function responseWith(ResponseInterface $message): ResponseContract
+    {
+        return new Response($message);
+    }
 }
--- src/Response.php
+++ src/Response.php
@@ -2,9 +2,85 @@
 
 namespace AimanDaniel\ToyyibPay;
 
-use \Laravie\Codex\Response as BaseResponse;
+use Laravie\Codex\Exceptions\NotFoundException;
+use Laravie\Codex\Exceptions\UnauthorizedException;
+use Laravie\Codex\Response as BaseResponse;
 
-abstract class Response extends BaseResponse
+class Response extends BaseResponse
 {
-    // 
+    /**
+     * Get body.
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return trim(parent::getBody());
+    }
+
+    /**
+     * Validate the response object.
+     *
+     * @return $this
+     */
+    public function validate()
+    {
+        parent::validate();
+
+        $this->abortIfInvalidCategoryCode();
+
+        return $this;
+    }
+
+    /**
+     * Check if response is unauthorized.
+     *
+     * @return bool
+     */
+    public function isUnauthorized(): bool
+    {
+        $body = trim($this->getBody());
+
+        $unauthorizedFlag = (false !== strpos("[KEY-DID-NOT-EXIST-OR-USER-IS-NOT-ACTIVE]", $body));
+
+        return parent::isUnauthorized() || $unauthorizedFlag;
+    }
+
+    /**
+     * Validate for unauthorized request.
+     *
+     * @throws \Laravie\Codex\Exceptions\UnauthorizedException
+     *
+     * @return void
+     */
+    public function abortIfRequestUnauthorized(): void
+    {
+        if ($this->isUnauthorized()) {
+            throw new UnauthorizedException($this, 'Invalid secret key');
+        }
+    }
+
+    /**
+     * Check if the category code is invalid
+     *
+     * @return boolean
+     */
+    public function isInvalidCategoryCode(): bool
+    {
+        return strpos("[CATEGORY-NOT-MATCH]", trim($this->getBody())) !== false;
+    }
+
+    /**
+     * Validate for invalid category code request.
+     *
+     * @throws \Laravie\Codex\Exceptions\NotFoundException
+     *
+     * @return void
+     */
+    public function abortIfInvalidCategoryCode(): void
+    {
+        if ($this->isInvalidCategoryCode()) {
+            throw new NotFoundException($this, 'Invalid category code');
+        }
+    }
 }
